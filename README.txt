41d20e8518bcf80f77a652b626721fce0a794ff8
Jesús Barros <jesus.barros.castro@gmail.com>
2015-02-14 23:16:43 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+Installation instructions:
+
+- Download and install NodeJS from http://nodejs.org.
+- Download and install MongoDB from http://www.mongodb.org/downloads.
+- In the root project directory (the one where package.json is), execute the following instruction on your command line: npm install. This will download some javascript modules for the application to run.
+- Start your database server by executing: mongod (or <MONGODB_DIR>/bin/mongod).
+- Start your web server by executing, in the root project directory: node app.js
+- View in your browser at: http://localhost:8080
+
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,45 @@
+/**
+ * Created by jbarros on 03/02/2015.
+ */
+
+angular.module('angularGDLEditor', []).controller('mainController', mainController)
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all guides and show them
+    $http.get('/api/guides')
+        .success(function(data) {
+            $scope.guides = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createGuide = function() {
+        $http.post('/api/guides', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.guides = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a guide after checking it
+    $scope.deleteGuide = function(id) {
+        $http.delete('/api/guides/' + id)
+            .success(function(data) {
+                $scope.guides = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
