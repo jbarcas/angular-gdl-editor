c314254549e285e8bf3d09d09d718920544e8453
Jesús Barros <jesus.barros.castro@gmail.com>
2015-06-14 23:53:44 +0200

    Updated drag and drop features

--- public/controllers/dragDropController.js
+++ public/controllers/dragDropController.js
@@ -10,11 +10,10 @@ app.controller('DragDropController', function($scope) {
 
     $scope.drop = true;
 
-    $scope.list1 = {title: 'Archetype instantiation', drag: true};
-    $scope.list2 = {title: 'Element instantiation'};
-    $scope.list3 = {title: 'Predicate (DataValue)'};
-    $scope.list4 = {drag: true};
-    $scope.list5 = [];
+    $scope.list0 = [{title: 'Archetype instantiation', drag: true}];
+    $scope.list1 = [{title: 'Element instantiation'}, {title: 'Predicate (DataValue)'}, {title: 'Predicate (Function)'}, {title: 'Predicate (Exists)'}, {title: 'Predicate (Expression)'} ];
+    $scope.list2 = {drag: true};
+    $scope.list3 = [];
 
     $scope.dropCallback = function(event, ui, item) {
         item.drag = false;
