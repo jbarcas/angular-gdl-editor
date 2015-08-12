160df6089f652020c8009b2f5e22cd9061b8a2dd
Jesús Barros <jesus.barros.castro@gmail.com>
2015-08-12 04:06:24 +0200

    Added the remaining functionalities of the 'description' tab. View restructuring

--- public/configuration.js
+++ public/configuration.js
@@ -4,5 +4,4 @@
 
 //FIXME: This URL should be enviroment dependent and specified at building time
 angular.module('configuration', [])
-    .constant('API_URL','http://localhost:8080/km/admin')
-    .constant('language', 'en');
+    .constant('API_URL','http://localhost:8080/km/admin');
